07d9d767e1fa065d6d71e433869e0b36d1aa4165
Robert Safin <robertsafin98@gmail.com>
2023-04-05 12:44:18 +0800

    set up redux store

---
+++ src/store/index.js
@@ -0,0 +1,33 @@
+
+import { createStore } from 'redux';
+
+const counterReducer = (state = {counter: 0}, action) => {
+
+  if (action.type === 'INCREMENT') {
+    return {
+      counter: state.counter + 1,
+    }
+  }
+
+  if (action.type === 'DECREMENT') {
+    return {
+      counter: state.counter - 1,
+    }
+  }
+
+  return state;
+};
+
+const store = createStore(counterReducer);
+
+export default store;
+
+// const counterSubscriber = () => {
+//   const latestState = store.getState();
+//   console.log(latestState);
+// };
+
+
+// store.subscribe(counterSubscriber);
+
+// store.dispatch({ type: 'INCREMENT' });
